1816e3ae614f138f6b49e767103f644b500d30ef
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2024-08-26 16:32:49 +0300

    add useSearchParams and useEffect

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { searchMovies } from '../../api/API';
 import MovieList from '../../components/MovieList/MovieList';
 import styles from './MoviesPage.module.css';
 
 const MoviesPage = () => {
-    const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('query') || '';
+
+    useEffect(() => {
+        if (query) {
+            searchMovies(query).then(setMovies);
+        }
+    }, [query]);
 
     const handleSearch = (e) => {
         e.preventDefault();
-        searchMovies(query).then(setMovies);
+        const form = e.currentTarget;
+        const newQuery = form.elements.search.value.trim();
+
+        if (newQuery === '') {
+            alert('Please enter a valid search query');
+            return;
+        }
+
+        setSearchParams({ query: newQuery });
     };
 
     return (
@@ -17,8 +33,8 @@ const MoviesPage = () => {
             <form onSubmit={handleSearch}>
                 <input
                     type="text"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
+                    name="search"
+                    defaultValue={query}
                     placeholder="Search movies"
                 />
                 <button type="submit">Search</button>
